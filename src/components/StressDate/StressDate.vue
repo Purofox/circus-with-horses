6c311d4b4b7031af795c7caac0724a4222651702
purofox <mitliska@yandex.ru>
2021-02-02 16:24:39 +0300

    Added moment.js

---
+++ src/components/StressDate/StressDate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="moment-test">
+    1
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "StressDate"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/StressLevel.vue
+++ src/pages/StressLevel.vue
@@ -1,9 +1,22 @@
 <template>
-  <p>StressLevel</p>
+  <StressDate/>
 </template>
 
 <script>
+import StressDate from '../components/StressDate/StressDate.vue';
+import moment from 'moment';
   export default {
-    name: "StressLevel"
+    name: "StressLevel",
+    components: {
+      StressDate
+    },
+    mounted() {
+      const halloween = moment([2011, 9, 31]); // October 31st
+      console.log(halloween.fromNow());
+
+      const now = moment().add('days', 9);
+      moment.lang('ru');
+      console.log(now.format('dddd, MMMM DD YYYY'));
+    }
   };
 </script>
