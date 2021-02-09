d48da2cdd493e62530d4a8a9e6c7f10426d489db
purofox <mitliska@yandex.ru>
2021-02-09 15:13:17 +0300

    Fixed some issues

---
+++ src/components/StressTabs/StressTabs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="stress-container">
+    <keep-alive>
+      <div class="tab">1</div>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StressTabs"
+};
+</script>
+
+<style scoped>
+  .stress-container {
+    background: url("../../assets/stress-bg.jpg") no-repeat center;
+    background-size: cover;
+    height: 100%;
+    padding-top: 140px;
+  }
+</style>
