de070f2951cf1c479b23bf1358867881166e4cfd
purofox <mitliska@yandex.ru>
2021-02-15 08:23:24 +0300

    Added vue cookie js

--- src/components/Pomodoro/Pomodoro.vue
+++ src/components/Pomodoro/Pomodoro.vue
@@ -22,6 +22,8 @@
 
 <script>
 import BackToMain from '../BackToMain/BackToMain.vue';
+import $cookies from "core-js/internals/internal-state";
+
 export default {
     name: "Pomodoro",
     components: {
@@ -39,7 +41,7 @@ export default {
     },
 
     created() {
-      let timer_now = localStorage.getItem('timer_now');
+      let timer_now = $cookies.get('timer_now');
       if(timer_now < this.timer) {
         this.timer = timer_now;
         this.startTimer();
@@ -76,7 +78,7 @@ export default {
         return (time < 10 ? '0' : '') + time;
       },
       save_timer() {
-        localStorage.setItem('timer_now' , this.timer);
+        $cookies.set('timer_now' , this.timer);
       },
       playMethod() {
         const audio = new Audio('../../assets/audio/alarm-clock-bell.mp3');
--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App.vue';
 import VueLodash from 'vue-lodash';
 import lodash from 'lodash';
 import VueI18n from 'vue-i18n';
+import VueCookies from 'vue-cookies';
 
 Vue.use(Vuex);
 Vue.use(VueI18n);
 Vue.use(VueLodash, { name: 'custom' , lodash: lodash });
+Vue.use(VueCookies);
 
 Vue.config.productionTip = false;
 
